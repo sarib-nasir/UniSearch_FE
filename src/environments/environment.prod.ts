322bb72d6fcd1c2e57d639ac1952a8a27d00e475
sarib nasir <saribbinnasir@gmail.com>
2025-03-18 22:09:19 +0100

    changes in prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,28 +2,8 @@ export const environment = {
   production: true,
 
   // SIT
-
-  // // serverIP: 10.35.1.180
-  // frontBaseURL: 'https://wfsuat.soneribank.com',
-  // apiBaseURL: 'https://wfs-uat-be.soneribank.com',
-
-  //For Soner_WFS
-  //  frontBaseURL: 'http://119.152.241.152:89/',
-  //  apiBaseURL: 'http://119.152.241.152/Soneri_WFS_BE',
-
-//For RL_WFS
   frontBaseURL: 'http://ahsanaziz-001-site53.anytempurl.com/',
   apiBaseURL: 'http://ahsanaziz-001-site54.anytempurl.com',
-  // frontBaseURL: 'http://119.152.241.152:100/',
-  // apiBaseURL: 'http://119.152.241.152/Soneri_WFS_BE',
-
-  //Production
-// // serverIP: 10.35.1.180
-  // frontBaseURL: 'https://wfs.soneribank.com',
-  // // apiBaseURL: 'https://wfs-be.soneribank.com',
-  // frontBaseURL: 'http://119.152.241.152:112/',
-  // apiBaseURL: 'http://119.152.241.152/WFS_BE_V2',
-
 
   appKEY1: 'az1991ly08ix23',
   appKEY2: 'asdfewrewqrss323',
